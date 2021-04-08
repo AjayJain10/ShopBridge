cb418514f115c603fdc6f06afe0e8f25c7843b7f
AjayJain10 <ajayjain9832@gmail.com>
2021-04-09 01:26:03 +0530

    Added service

--- Shop-Bridge/src/app/app.component.ts
+++ Shop-Bridge/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonServiceService } from 'src/services/common-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Shop-Bridge';
+
+  constructor(private commonService: CommonServiceService) {  }
+
+  ngOnInit(): void {
+    this.commonService.getAllItems().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    });
+  }
+
 }
---
+++ Shop-Bridge/src/services/common-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonServiceService {
+
+  baseURL: string = 'http://dummy.restapiexample.com/api/v1/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllItems(): Observable<any> {
+    return this.http.get(this.baseURL + 'employees');
+  }
+
+}
